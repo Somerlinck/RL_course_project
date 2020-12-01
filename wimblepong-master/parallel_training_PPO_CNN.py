6801f3dc5fa093bc8d3d27dffd1745de6d499fc5
Somerlinck <leo.vuylsteker@imt-atlantique.net>
2020-12-01 16:15:02 +0200

    CNN model

--- wimblepong-master/parallel_training_PPO_CNN.py
+++ wimblepong-master/parallel_training_PPO_CNN.py
@@ -2,22 +2,31 @@ import torch
 import gym
 import numpy as np
 import argparse
-from wimblepong.PPO_agent import Agent, ActorCritic
+from wimblepong.CNN_PPO_agent import Agent, ActorCritic
 from parallel_env import ParallelEnvs
 
 
-def transform_observations(previous_observations, observations, x_arena_res=200, y_arena_res=200):
-    n_proc = observations.shape[0]
-    res = np.zeros((n_proc, x_arena_res * y_arena_res))
-    for i in range(n_proc):
-        mat = abs(2*observations[i].dot([0.07, 0.72, 0.21]) - previous_observations[i].dot([0.07, 0.72, 0.21])) \
-            if previous_observations is not None else np.zeros((y_arena_res, x_arena_res))
-        res[i] = mat.ravel()
+def states_cnn(observations_1, observations_2, observations_3, x_arena_res=200, y_arena_res=200):
+    nb_proc = observations_1.shape[0]
+    res = np.zeros((nb_proc, 3, y_arena_res, x_arena_res))
+    for i in range(nb_proc):
+        res[i, 0] = observations_1[i].dot([0.07, 0.72, 0.21])
+        res[i, 1] = observations_2[i].dot([0.07, 0.72, 0.21])
+        res[i, 2] = observations_3[i].dot([0.07, 0.72, 0.21])
+    return res
+
+
+def state_cnn(observation_1, observation_2, observation_3, x_arena_res=200, y_arena_res=200):
+    res = np.zeros((1, 3, y_arena_res, x_arena_res))
+
+    res[0, 0] = observation_1.dot([0.07, 0.72, 0.21])
+    res[0, 1] = observation_2.dot([0.07, 0.72, 0.21])
+    res[0, 2] = observation_3.dot([0.07, 0.72, 0.21])
     return res
 
 
 # Policy training function
-def train(env_name, print_things=True, train_run_id=0, train_timesteps=200000, update_steps=50):
+def train(env_name, print_things=True, train_run_id=0, train_timesteps=500000, update_steps=50, load=False):
     # Create a Gym environment
     # This creates 64 parallel envs running in 8 processes (8 envs each)
     env = ParallelEnvs(env_name, processes=8, envs_per_process=8)
@@ -30,17 +39,27 @@ def train(env_name, print_things=True, train_run_id=0, train_timesteps=200000, u
     # Instantiate agent and its policy
     AC_old = ActorCritic(observation_space_dim, action_space_dim)
     AC = ActorCritic(observation_space_dim, action_space_dim)
-    agent = Agent(AC_old, AC)
 
-    # Arrays to keep track of rewards
-    reward_history, timestep_history = [], []
-    average_reward_history = []
+    if load:
+        print("Loading model")
+        checkpoint = torch.load('Model/modelPG_last.mdl')
+        model = checkpoint['model']
+        # model = checkpoint
+        optimizer = checkpoint['optimizer']
+        AC.load_state_dict(model)
+        agent = Agent(AC_old, AC)
+        agent.optimizer.load_state_dict(optimizer)
+    else:
+        agent = Agent(AC_old, AC)
 
     # Run actual training
     # Reset the environment and observe the initial state
-    observations = env.reset()
-    previous_observations = None
-    next_states = transform_observations(previous_observations, observations, x_arena_res, y_arena_res)
+    observations_1 = env.reset()
+    nb_proc = observations_1.shape[0]
+    observations_3 = np.zeros((nb_proc, y_arena_res, x_arena_res, 3))
+    observations_2 = np.zeros((nb_proc, y_arena_res, x_arena_res, 3))
+
+    next_states = states_cnn(observations_1, observations_2, observations_3, x_arena_res, y_arena_res)
 
     avg_rewards = None
 
@@ -49,11 +68,12 @@ def train(env_name, print_things=True, train_run_id=0, train_timesteps=200000, u
         states = next_states
         # Get action from the agent
         actions, log_probs = agent.get_action(states)
-        previous_observations = observations
 
         # Perform the action on the environment, get new state and reward
-        observations, rewards, dones, info = env.step(actions.detach().numpy())
-        next_states = transform_observations(previous_observations, observations, x_arena_res, y_arena_res)
+        observations_3 = observations_2
+        observations_2 = observations_1
+        observations_1, rewards, dones, info = env.step(actions.detach().numpy())
+        next_states = states_cnn(observations_1, observations_2, observations_3, x_arena_res, y_arena_res)
 
         if (rewards != 0).sum() != 0:
             if avg_rewards is None:
@@ -69,12 +89,16 @@ def train(env_name, print_things=True, train_run_id=0, train_timesteps=200000, u
             agent.update(0)
             avg_rewards = None
 
-        if timestep % 10000 == 0:
-          model_name = "modelPG_" + str(timestep)
-          path = f'Model/{model_name}.mdl'
-          print(f'Saving {model_name} model...')
-          torch.save(agent.policy.state_dict(), path)
-          print(f'{model_name} saved successfully.')
+        if timestep % 50000 == 0:
+            model_name = "modelPG_" + str(timestep)
+            path = f'Model/{model_name}.mdl'
+            print(f'Saving {model_name} model...')
+            checkpoint = {
+                'model': agent.policy.state_dict(),
+                'optimizer': agent.optimizer.state_dict()
+            }
+            torch.save(checkpoint, path)
+            print(f'{model_name} saved successfully.')
 
 
 # Function to test a trained policy
@@ -95,16 +119,21 @@ def test(env_name, episodes, params, render):
     test_reward, test_len = 0, 0
     for ep in range(episodes):
         done = False
-        observation = env.reset()
+        observation_1 = env.reset()
+        observation_3 = np.zeros((y_arena_res, x_arena_res, 3))
+        observation_2 = np.zeros((y_arena_res, x_arena_res, 3))
         while not done:
+            state = state_cnn(observation_1, observation_2, observation_3)
             # Similar to the training loop above -
             # get the action, act on the environment, save total reward
             # (evaluation=True makes the agent always return what it thinks to be
             # the best action - there is no exploration at this point)
-            action, _ = agent.get_action(observation, evaluation=True)
-            observation, reward, done, info = env.step(action.detach().cpu().numpy())
+            action, _ = agent.get_action(state, evaluation=True)
+            observation_3 = observation_2
+            observation_2 = observation_1
+            observation_1, reward, done, info = env.step(action.detach().cpu().numpy())
 
-            if render:
+            if False:
                 env.render()
             test_reward += reward
             test_len += 1
@@ -115,8 +144,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--test", "-t", type=str, default=None, help="Model to be tested")
     parser.add_argument("--env", type=str, default="WimblepongVisualSimpleAI-v0", help="Environment to use")
-    parser.add_argument("--train_timesteps", type=int, default=1000, help="Number of timesteps to train for")
+    parser.add_argument("--train_timesteps", type=int, default=500000, help="Number of timesteps to train for")
     parser.add_argument("--render_test", action='store_true', help="Render test")
+    parser.add_argument("--load", action='store_true', help="load a saved model")
     args = parser.parse_args()
 
     # If no model was passed, train a policy from scratch.
---
+++ wimblepong-master/wimblepong/CNN_PPO_agent.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+from torch.distributions import Categorical
+import torch.nn.functional as F
+
+
+def init_layer(m):
+    weight = m.weight.data
+    weight.normal_(0, 1)
+    weight *= 1.0 / torch.sqrt(weight.pow(2).sum(1, keepdim=True))
+    nn.init.constant_(m.bias.data, 0)
+    return m
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_space, action_space):
+        super().__init__()
+
+        self.conv = torch.nn.Sequential(
+            nn.Conv2d(3, 32, 8),  # _x_x32
+            nn.ReLU(),
+            nn.MaxPool2d(4),  # 98x98x16
+            nn.Conv2d(32, 64, 4),  # _x_x16
+            nn.ReLU(),
+            torch.nn.MaxPool2d(4),  # 48x48x16
+            torch.nn.Conv2d(64, 64, 3),  # 46x46x8
+            nn.ReLU(),
+            torch.nn.MaxPool2d(4)
+        )
+
+        # We do not want to select action yet as that will be probablistic.
+        self.actor = nn.Sequential(
+                init_layer(nn.Linear(256, 64)),
+                nn.Tanh(),
+                init_layer(nn.Linear(64, 64)),
+                nn.Tanh(),
+                init_layer(nn.Linear(64, action_space)),
+                nn.Softmax(dim=-1)
+            )
+
+        self.critic = nn.Sequential(
+                init_layer(nn.Linear(256, 64)),
+                nn.Tanh(),
+                init_layer(nn.Linear(64, 64)),
+                nn.Tanh(),
+                init_layer(nn.Linear(64, 1)),
+            )
+
+    def forward(self, inputs):
+        feature_maps = self.conv(inputs)
+        n_proc = feature_maps.shape[0]
+        return self.actor(feature_maps.view(n_proc, 256)), self.critic(feature_maps.view(n_proc, 256))
+
+    def evaluate(self, state, action):
+        aprobs, state_value = self.forward(state)
+        dist = Categorical(aprobs)
+        a_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+        return a_logprobs, torch.squeeze(state_value), dist_entropy
+
+
+class Agent(object):
+    def __init__(self, AC_old, AC):
+        self.train_device = "cpu"
+        self.policy = AC.to(self.train_device)
+        self.old_policy = AC_old.to(self.train_device)
+        self.old_policy.load_state_dict(self.policy.state_dict())
+
+        self.optimizer = torch.optim.Adam(AC.parameters(), lr=5e-3)
+        self.gamma = 0.98
+        self.K_epochs = 3
+        self.epsilon_clip = 0.2
+
+        self.states = []
+        self.next_states = []
+        self.log_probs = []
+        self.rewards = []
+        self.actions = []
+        self.dones = []
+
+    def get_action(self, observation, evaluation=False):
+        x = torch.from_numpy(observation).float().to(self.train_device)
+        aprob, _ = self.old_policy.forward(x)
+        distribution = Categorical(aprob)
+
+        if evaluation:
+            action = aprob.argmax()
+        else:
+            action = distribution.sample()
+
+        return action, distribution.log_prob(action)
+
+    def store_outcome(self, state_array, log_prob, action, reward, next_state_array, done):
+        self.states.append(torch.from_numpy(state_array).float())
+        self.next_states.append(torch.from_numpy(next_state_array).float())
+        self.log_probs.append(log_prob)
+        self.actions.append(action)
+        self.rewards.append(torch.Tensor([reward]))
+        self.dones.append(torch.Tensor([done]).int())
+
+    def update(self, episode_number):
+        rewards = torch.stack(self.rewards, dim=0).to(self.train_device).squeeze(-1)
+        old_states = torch.stack(self.states, dim=0).to(self.train_device).squeeze(-1)
+        next_states = torch.stack(self.next_states, dim=0).to(self.train_device).squeeze(-1)
+        old_actions = torch.stack(self.actions, dim=0).to(self.train_device).squeeze(-1)
+        old_log_probs = torch.stack(self.log_probs, dim=0).to(self.train_device).squeeze(-1)
+        dones = torch.stack(self.dones, dim=0).to(self.train_device).squeeze(-1)
+        self.states, self.actions, self.log_probs, self.rewards, self.next_states, self.dones = [], [], [], [], [], []
+
+        # Compute discounted rewards
+        _, next_state_values, _ = self.policy.evaluate(next_states, old_actions)
+        next_state_values = next_state_values.detach()
+        returns = rewards + self.gamma * next_state_values * (1-dones)
+
+        # Optimize policy for K epochs
+        for _ in range(self.K_epochs):
+            # Evaluate old actions and values
+            log_probs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+
+            # pi_theta / pi_theta_old
+            ratios = torch.exp(log_probs - old_log_probs.detach())
+
+            # Surrogate Loss
+            advantages = returns - state_values.detach()
+            surrogate1 = ratios * advantages
+            surrogate2 = torch.clamp(ratios, min=1-self.epsilon_clip, max=1+self.epsilon_clip) * advantages
+            loss = - torch.min(surrogate1, surrogate2).mean() + F.mse_loss(returns, state_values) - 0.01*dist_entropy.mean()
+
+            # Update network parameters
+            if episode_number % 50 == 0:
+                loss.backward()
+            if episode_number % 200 == 0:
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+        # Update weights of the old policy
+        self.old_policy.load_state_dict(self.policy.state_dict())
